e81d339fb11910b550b403b081a795da16561267
Ben Rich <beri9208@colorado.edu>
2024-05-08 16:38:57 -0600

    docustrings

--- funcs/pdos_funcs.py
+++ funcs/pdos_funcs.py
@@ -10,6 +10,34 @@ from ase.dft.dos import linear_tetrahedron_integration as lti
 
 
 def get_cheap_pdos(idcs, path, data=None, res=0.01, sig=0.00001, orbs=None, Erange=None, spin_pol=False):
+    """
+    :param idcs: list[int]
+        List of atom indices to plot pDOS of (0-based indices)
+    :param path: str or path
+        Full path for directory containing output files from calculation
+    :param data: tuple
+        Output dictionary from 'parse_data' function found in helpers.data_parsers
+    :param res: float
+        dE for evenly spaced energy array (in Hartree)
+    :param sig: float
+        Controls smearing amplitude of gaussian smearings
+    :param orbs: list[str]
+        List orbitals to include in pDOS (includes all if None)
+            - ie orbs = ["s"] would include only s orbitals,
+                orbs = ["d"] would include only d orbitals,
+                orbs = ["px"] would include only px orbitals
+    :param Erange: np.ndarray[float] of shape (,N)
+        Array of energy values to evaluate pDOS for (Hartree)
+    :param spin_pol: bool
+        If true, pDOS return array returned as (2,N) array, where first index belongs to spin
+    :return Erange, pdos:
+        :Erange: np.ndarray[float] of shape (,N)
+        :pdos:
+            if spin_pol:
+                np.ndarray[float] of shape (2,N)
+            else:
+                np.ndarray[float] of shape (,N)
+    """
     Erange, weights_sabcj, E_sabcj = get_pdos_pieces(idcs, path, data, res, orbs, Erange)
     cs = get_cheap_pcohp_helper(Erange, E_sabcj, weights_sabcj, sig)
     pdos = cs_formatter(cs, spin_pol)
@@ -17,6 +45,32 @@ def get_cheap_pdos(idcs, path, data=None, res=0.01, sig=0.00001, orbs=None, Eran
 
 
 def get_tetr_pdos(idcs, path, data=None, res=0.01, orbs=None, Erange=None, spin_pol=False):
+    """
+    :param idcs: list[int]
+        List of atom indices to plot pDOS of (0-based indices)
+    :param path: str or path
+        Full path for directory containing output files from calculation
+    :param data: tuple
+        Output dictionary from 'parse_data' function found in helpers.data_parsers
+    :param res: float
+        dE for evenly spaced energy array (in Hartree)
+    :param orbs: list[str]
+        List orbitals to include in pDOS (includes all if None)
+            - ie orbs = ["s"] would include only s orbitals,
+                orbs = ["d"] would include only d orbitals,
+                orbs = ["px"] would include only px orbitals
+    :param Erange: np.ndarray[float] of shape (,N)
+        Array of energy values to evaluate pDOS for (Hartree)
+    :param spin_pol: bool
+        If true, pDOS return array returned as (2,N) array, where first index belongs to spin
+    :return Erange, pdos:
+        :Erange: np.ndarray[float] of shape (,N)
+        :pdos:
+            if spin_pol:
+                np.ndarray[float] of shape (2,N)
+            else:
+                np.ndarray[float] of shape (,N)
+    """
     Erange, weights_sabcj, E_sabcj = get_pdos_pieces(idcs, path, data, res, orbs, Erange)
     atoms = get_atoms(path)
     cs = []
